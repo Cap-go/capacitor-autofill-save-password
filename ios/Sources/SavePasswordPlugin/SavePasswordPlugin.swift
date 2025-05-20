53e8ad35e9367bd613d4872fea0439a24f155f0e
Martin Donadieu <martindonadieu@gmail.com>
2025-05-20 21:45:19 +0100

    fix: add missing class

--- ios/Sources/SavePasswordPlugin/SavePasswordPlugin.swift
+++ ios/Sources/SavePasswordPlugin/SavePasswordPlugin.swift
@@ -24,3 +24,29 @@ public class SavePasswordPlugin: CAPPlugin, CAPBridgedPlugin {
         }
     }
 }
+
+class LoginScreenViewController: UIViewController {
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame.size.width = 1
+        textField.frame.size.height = 1
+        textField.textContentType = .username
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame.size.width = 1
+        textField.frame.size.height = 1
+        textField.textContentType = .password
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        view.addSubview(usernameTextField)
+        view.addSubview(passwordTextField)
+        usernameTextField.becomeFirstResponder()
+    }
+}
