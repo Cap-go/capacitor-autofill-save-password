98cff808fb63b3e815fe3f6b8c9fb3698c3bf22e
WcaleNieWolny <isupermichael007@gmail.com>
2025-05-23 06:28:50 +0200

    test: demo app for v6

---
+++ example-app-v6/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ee.forgr.capgo.capacitor_autofull_save_password_v6',
+  appName: 'password fill v6',
+  webDir: 'dist'
+};
+
+export default config;
