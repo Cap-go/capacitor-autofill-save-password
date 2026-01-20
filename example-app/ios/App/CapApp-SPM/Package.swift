34114226ac68a84128ce4b0e77bde303cdefdba0
Martin Donadieu <martindonadieu@gmail.com>
2026-01-20 03:51:02 +0000

    chore: use capacitor-swift-pm from 8.0.0

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(name: "CapgoCapacitorAutofillSavePassword", path: "../../../node_modules/.bun/@capgo+capacitor-autofill-save-password@file+../node_modules/@capgo/capacitor-autofill-save-password")
     ],
     targets: [
